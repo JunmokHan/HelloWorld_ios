467e7017f66ea3f184e4fe977781982b93af9b1b
han <kalissma790@naver.com>
2017-02-05 03:24:01 +0900

    cm1

--- jaljali/ViewController.swift
+++ jaljali/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var lbHello: UILabel!
+    
+    @IBOutlet weak var txtName: UITextField!
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
